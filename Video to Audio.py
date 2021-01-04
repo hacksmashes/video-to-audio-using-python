c3aaaeb7f895dbccd52d1257c0cf53e25f276808
Logesh.S <76871611+Everything-with-python@users.noreply.github.com>
2021-01-04 22:38:54 +0530

    Update and rename vdo to aud.py to Video to Audio.py

--- Video to Audio.py
+++ Video to Audio.py
@@ -1,9 +1,7 @@
 from moviepy.editor import *
 
-file_name="intro"                                                        # give the file name correctly
+file_name="intro"                                    # give the file name correctly
 my_clip=VideoFileClip(f"{file_name}.mp4")            # don't forget to mention the extension ( .mp4 )
-my_clip.audio.write_audiofile(f"{file_name}.mp3") # don't forget to mention the extension ( .mp3 )
+my_clip.audio.write_audiofile(f"{file_name}.mp3")    # don't forget to mention the extension ( .mp3 )
 print(f"Audio File Name : {my_clip.filename}")
 print(f"Audio File Duration : {my_clip.duration}")
-
-
